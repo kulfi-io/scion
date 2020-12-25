4afc5dd23296f812e583197e13cf8e34472d1862
Ashish Chatterjee <ashish@kulfi.io>
2020-12-24 22:52:13 -0800

    fixed typo

--- api/src/db/config/config.js
+++ api/src/db/config/config.js
@@ -7,7 +7,7 @@ dotenv.config({
 module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     schema: process.env.DB_SCHEMA,
     searchPath: process.env.DB_SEARCH_PATH,
